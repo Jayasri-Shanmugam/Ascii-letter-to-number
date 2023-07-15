b167e3cff7c14d33aa3ab53fdc09a8f0be5e8260
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:42:26 +0530

    Add files via upload

---
+++ asciilettertonumber.java
@@ -0,0 +1,16 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+public class asciilettertonumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char letter=sc.next().charAt(0);
+		int a=(int)letter;
+		System.out.println(a);
+		sc.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
